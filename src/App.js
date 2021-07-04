e2ce56b9551e88c4dda1e0a211a9acff76324967
Rodrigo Novaes <rod_novaes@hotmail.com>
2021-07-04 15:07:16 +1200

    final commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, incrementByAmount } from './redux/counter'
 import './App.css'
 
 function App () {
+  const { count } = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
   return (
     <div className="App">
-      Hello
+      <h1> The count is: {count}</h1>
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(decrement())}>decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(33))}>
+        Increment by 33
+      </button>
     </div>
   )
 }
